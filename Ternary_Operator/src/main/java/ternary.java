764339000559da86bf2187ae8b664274a4179d1c
MohammedMaisoon <159471745+MohammedMaisoon@users.noreply.github.com>
2024-07-27 14:12:42 +0530

    Update ternary.java

--- Ternary_Operator/src/main/java/ternary.java
+++ Ternary_Operator/src/main/java/ternary.java
@@ -4,7 +4,6 @@ public class ternary {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your age: ");
         int age = input.nextInt();
-        String ageInput = input.nextLine();
         String ter = age >= 18 ? "You are Eligible to vote" : "You are not eligible to vote";
         System.out.println(ter);
     }}
